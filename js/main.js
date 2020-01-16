88cfc826c57f5403103dce7643390548d5290514
FreeSoulll <45289929+FreeSoulll@users.noreply.github.com>
2020-01-16 17:34:28 +0300

    Доделал layout

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var DOMstrings = {
     parent: '.gallery_items',
     galleryContainer: '.gallery_container',
     imgGallery: '.container_small_img',
-    popup: '.popup'
+    popup: '.popup',
+    smallImg: '.small_img'
 };
 
 var album = {
@@ -14,28 +15,49 @@ var album = {
     albumId: function gallery() {
         fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + this.id)
             .then(response => response.json())
-            .then(json => json.forEach(function(obj) {
+            .then(json => json.forEach(function (obj) {
                 var img = document.createElement("img");
-                link = document.createElement("div")
+                var link = document.createElement("div");
                 img.src = obj.thumbnailUrl;
                 link.setAttribute('data-images', obj.url);
                 link.classList.add('container_small_img');
                 img.classList.add('small_img');
                 document.querySelector(DOMstrings.parent).appendChild(link).appendChild(img);
-                link.addEventListener('click', function() {
+            }))
+    },
+
+    overlay: function overlay() {
+        fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + this.id)
+            .then(response => response.json())
+            .then(() => {
+                var overlay = document.createElement('div');
+                overlay.classList.add('overlay');
+                document.querySelector('.gallery_block').appendChild(overlay);
+            })
+    },
+
+    bigImg: function bigImg() {
+        fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + this.id)
+            .then(response => response.json())
+            .then(() => {
+                document.querySelectorAll(DOMstrings.imgGallery).forEach(elem => elem.addEventListener('click', function () {
+                    var popup = document.createElement("img");
                     document.querySelector(DOMstrings.overlay).classList.add('show');
-                    popup = document.createElement("img");
-                    popup.src = obj.url;
+                    popup.src = this.getAttribute('data-images');
                     popup.classList.add('big_img');
                     popup.classList.add('show');
                     document.querySelector(DOMstrings.galleryContainer).appendChild(popup);
-                });
-                document.querySelector(DOMstrings.overlay).addEventListener('click', function() {
+                }));
+                document.querySelector(DOMstrings.overlay).addEventListener('click', function () {
                     document.querySelector(DOMstrings.overlay).classList.remove('show');
+                    var popup = document.querySelector('.big_img');
+                    document.querySelector('.gallery_block').removeChild(document.querySelector(DOMstrings.overlay));
                     document.querySelector(DOMstrings.galleryContainer).removeChild(popup);
+                    console.log(popup);
                 });
-            }))
+            })
     },
+
     title: function header() {
         fetch('https://jsonplaceholder.typicode.com/albums/' + this.id)
             .then(response => response.json())
@@ -61,25 +83,30 @@ var album = {
 
 function init() {
     album.albumId();
+    album.overlay();
     album.title();
     album.albumLength();
-};
+    album.bigImg();
+
+}
+
 init();
 
 function click() {
-    document.querySelector(DOMstrings.next).addEventListener('click', function() {
+    document.querySelector(DOMstrings.next).addEventListener('click', function () {
         while (document.querySelector(DOMstrings.parent).firstChild) {
             document.querySelector(DOMstrings.parent).removeChild(document.querySelector(DOMstrings.parent).firstChild);
         }
         album.id++;
         init();
     });
-    document.querySelector(DOMstrings.prev).addEventListener('click', function() {
+    document.querySelector(DOMstrings.prev).addEventListener('click', function () {
         while (document.querySelector(DOMstrings.parent).firstChild) {
             document.querySelector(DOMstrings.parent).removeChild(document.querySelector(DOMstrings.parent).firstChild);
         }
         album.id--;
         init();
     })
-};
+}
+
 click();
